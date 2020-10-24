c88a0537c96dfa4641678f9278136b597af32fd8
msheezi <marshall.randolph@gmail.com>
2020-10-24 10:09:37 -0700

    Commit prime factors in range

--- codingchallenges/primefactors.js
+++ codingchallenges/primefactors.js
@@ -1,12 +1,8 @@
 
 const getPrimeFactors = (num,high)=>{
     let result = []
-
     let i = 1
-   
-
     while( num**i <= high){
-        // console.log(num**i)
         result.push(num**i)
         i++
     }
@@ -20,11 +16,11 @@ const twoProds = (arr, target)=> {
             let potentialMatch = arr[left] * arr[right]
             if(potentialMatch>target)right-=1
             else if (potentialMatch<target)left += 1
-            else {
-                count+=1
+            else if (potentialMatch===target) {
+                console.log(potentialMatch, arr[left],arr[right], arr)
                 left += 1
                 right -= 1
-                console.log(potentialMatch, arr[left],arr[right])
+                count+=1
             }
         }
         return count
@@ -33,17 +29,22 @@ const twoProds = (arr, target)=> {
 
 const primeFactorsInRange = (low,high)=>{
     let count = 0
-    let resultArray = getPrimeFactors(3,high).concat(getPrimeFactors(5,high))
+    let resultArray =  getPrimeFactors(3,high).concat(getPrimeFactors(5,high)) // o(n)
+    // let resultArray = [1].concat( getPrimeFactors(3,high)).concat(getPrimeFactors(5,high)) // o(n)
 
-    let sorted = resultArray.sort((a,b)=> a-b)
-    console.log(sorted)
-    for (let i=low;i<=high;i++){
-        count += twoProds(sorted, i)
+    let sorted = resultArray.sort((a,b)=> a-b) // log n
+    for (let i=low;i<=high;i++){ //o(n) - essentially a nested loop
+        count += twoProds(sorted, i) 
     }
     return count
 }
 
 
+//time complexith n^2 log n
+//space n^2
+
 console.log(getPrimeFactors(3,200,455))
 console.log(getPrimeFactors(5,200,455))
 console.log(primeFactorsInRange(200,400))
+// console.log(primeFactorsInRange(0,755000))
+console.log(primeFactorsInRange(0,10))
